cd31dd2fa3c4fcdf299cd39210b26b3a77d2596b
GraemeOsborne <132292961+GraemeOsborne@users.noreply.github.com>
2023-05-06 09:09:27 +0100

    Footstep timing fixed
    Footstep timing fixed

--- Assets/Scripts/FootstepPlayer.cs
+++ Assets/Scripts/FootstepPlayer.cs
@@ -9,6 +9,12 @@ public class FootstepPlayer : MonoBehaviour
     // The FMOD event instance
     private FMOD.Studio.EventInstance footstepsInstance;
 
+    // The time at which the last footsteps were played
+    private float lastFootstepTime;
+
+    // The interval in seconds at which the footsteps should be played
+    private float footstepInterval = 0.15f;
+
     void Start()
     {
         // Create an instance of the FMOD event
@@ -20,8 +26,14 @@ public class FootstepPlayer : MonoBehaviour
         // Check if any of the WASD keys are pressed
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
-            // Play the FMOD event
-            footstepsInstance.start();
+            // Check if the interval time has elapsed since the last footsteps were played
+            if (Time.time - lastFootstepTime >= footstepInterval)
+            {
+                // Play the FMOD event
+                footstepsInstance.start();
+                // Update the lastFootstepTime to the current time
+                lastFootstepTime = Time.time;
+            }
         }
     }
 
@@ -31,5 +43,3 @@ public class FootstepPlayer : MonoBehaviour
         footstepsInstance.release();
     }
 }
-
-
